eca19e9ce076d922a0ae5329153a1e578d64b0c8
Mariam <127440951+Stchcm@users.noreply.github.com>
2023-05-03 12:55:25 +0200

    Update test_Entry.py

---
+++ Tests/test_Entry.py
@@ -0,0 +1,44 @@
+import unittest
+from datetime import date
+from entry import Entry
+
+
+class TestEntry(unittest.TestCase):
+
+    
+    def setUp(self):
+        self.entry = Entry(date.today(), "happy", "Today was a great day!", "My Happy Day")
+
+    
+    def test_get_id(self):
+        self.assertIsNone(self.entry.get_id())
+        self.entry.set_id(1)
+        self.assertEqual(self.entry.get_id(), 1)
+
+    
+    def test_get_date(self):
+        self.assertEqual(self.entry.get_date(), date.today())
+
+    
+    def test_set_date(self):
+        new_date = date(2021, 5, 3)
+        self.entry.set_date(new_date)
+        self.assertEqual(self.entry.get_date(), new_date)
+
+    
+    def test_get_mood(self):
+        self.assertEqual(self.entry.get_mood(), "happy")
+
+    
+    def test_set_mood(self):
+        self.entry.set_mood("sad")
+        self.assertEqual(self.entry.get_mood(), "sad")
+
+    
+    def test_get_text(self):
+        self.assertEqual(self.entry.get_text(), "Today was a great day!")
+
+    
+    def test_set_text(self):
+        self.entry.set_text("Today was a terrible day!")
+        self.assertEqual(self.entry.get_text(), "Today was a terrible day!")
